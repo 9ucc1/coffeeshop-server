9fa594fc6325a0e15447abd2705342f3e5c8bffe
Gucci Fan <gucc@umich.edu>
2023-01-29 20:42:37 -0500

    adding seed data and beginnings of routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,22 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  #get "/shops" do
+  #  { Shop.all }.to_json
+  #end
+
+  get '/shops' do
+    shops = Shop.all.order(:name)
+    #.limit(10)
+    shops.to_json
+  end
+
+  get '/shops/:id' do
+    shop = Shop.find(params[:id])
+
+    shop.to_json(only: [:id, :name, :location], include: {
+      drinks: { only: [:name, :price]}  
+    })
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,44 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+Shop.create([
+  {
+    name: "Chapelure",
+    location: "900 Hagadorn Rd",
+    id: 1
+  },
+  {
+    name: "Strange Matter Coffee",
+    location: "123 Apple Ln",
+    id: 2
+  },
+  {
+    name: "Hudson Hill",
+    location: "456 Pearl Ave",
+    id: 3
+  },
+  {
+    name: "Lab Cafe",
+    location: "100 Liberty St",
+    id: 4
+  }
+])
+
+Drink.create([
+    {
+        name: "Orange Mocha",
+        shop_id: 1,
+        decaf: false,
+        price: 4.50,
+        ingredients: "orange syrup, espresso, frothed milk",
+    },
+    {
+      name: "Honey Lavender Latte",
+      shop_id: 4,
+      decaf: false,
+      price: 3.99,
+      ingredients: "lavender syrup, espresso, frothed milk, honey",
+    }
+])
+
 puts "✅ Done seeding!"
